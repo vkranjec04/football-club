bc2e29e801147e527c4f8629e6735de044c09278
Viktor Kranjec <viktorkranjec2004@gmail.com>
2026-03-31 15:20:47 +0200

    Add project files.

---
+++ FootballClub/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FootballClub.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
